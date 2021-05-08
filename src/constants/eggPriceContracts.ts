a40cb2b319395436a9754caf26998233479a42d9
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-08 02:38:42 -0300

    Feat: initial commit

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0xf9d83e63Fc708716176C32aE51Db683F5B57D36C',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+}
+
+export default priceContracts
